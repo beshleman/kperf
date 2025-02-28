0dd611c5d1d6efb906d5ba625d177edc96ffa8fb
Stanislav Fomichev <sdf@fomichev.me>
2025-02-28 07:48:20 -0800

    devmem: don't pass valid iov_base
    In recvmsg, the kernel is doing:
    
    	if (skb_frags_readable()) {
    		/* copy payload into iov_base */
    	} else {
    		/* handle devmem if MSG_SOCK_DEVMEM is set */
    	}
    
    So it is currently possible for the misrouted non-devmem skbs to
    be received and accounted as devmem on the userspace side.
    
    To make sure we only receive devmem skbs, do the following:
    - set iov_base to NULL so skb_frags_readable fails with EFAULT
    - count the number of tokens received and abort when it's zero
    
    Also add MSG_DONTWAIT to be consistent with non-devmem recv path.
    
    Signed-off-by: Stanislav Fomichev <sdf@fomichev.me>

--- devmem.c
+++ devmem.c
@@ -961,10 +961,11 @@ ssize_t devmem_recv(int fd, struct connection_devmem *conn,
 {
 	struct msghdr msg = {};
 	struct iovec iov = {
-		.iov_base = rxbuf,
+		.iov_base = NULL,
 		.iov_len = chunk,
 	};
 	struct cmsghdr *cm;
+	int tokens = 0;
 	ssize_t n;
 	int ret;
 
@@ -972,7 +973,7 @@ ssize_t devmem_recv(int fd, struct connection_devmem *conn,
 	msg.msg_iovlen = 1;
 	msg.msg_control = conn->ctrl_data;
 	msg.msg_controllen = sizeof(conn->ctrl_data);
-	n = recvmsg(fd, &msg, MSG_SOCK_DEVMEM);
+	n = recvmsg(fd, &msg, MSG_DONTWAIT | MSG_SOCK_DEVMEM);
 	if (n < 0)
 		return n;
 
@@ -992,6 +993,13 @@ ssize_t devmem_recv(int fd, struct connection_devmem *conn,
 				return ret;
 		}
 
+		tokens++;
+	}
+
+	if (!tokens) {
+		warnx("devmem recvmsg returned no tokens");
+		errno = -EFAULT;
+		return -1;
 	}
 
 	return n;
