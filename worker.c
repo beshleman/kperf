beshleman/kperf

commit 5b58bf3ca7ada3edeb2ef45bbf97d5c106944ae6

devmem: add support for devmem TX
Add support for devmem TX.

Each worker sets up a private udmabuf and binds it to the netdev for
devmem TX. This means there is one udmabuf per connection/worker. The
buffers themselves are identical (i.e., memcpy(patbuf)) as is necessary
for validation.

Example output:

  ./client --src $SERVER1_IP --dst $SERVER2_IP --pin-off 1 -n 2 --devmem-rx
  --num-rx-queues 2 --udmabuf-size-mb 512  --devmem-tx

  I   client.c: Connected 1:cpu 1 | 1:cpu 81
  I   client.c: Connected 2:cpu 81 | 2:cpu 89
  client: == Source
  client:   Tx 94.153 Gbps (58857881600 bytes in 5001016 usec)
  client:   Tx 94.468 Gbps (59054489600 bytes in 5001016 usec)
  client:   Rx  0.000 Gbps (0 bytes in 5001016 usec)
  client:   Rx  0.000 Gbps (0 bytes in 5001016 usec)
  client:   TCP retrans reord rtt rttvar d_ce snd_wnd cwnd
  client:            97     0  88      3    0 58021888  680
  client:            97     0 114      2    0 63258624  583
  client:   net CPU  1: usr: 0.00% sys: 0.00% idle:55.64% iow: 0.00% irq:
  3.02% sirq:41.33%
  client:   app CPU  2: usr: 1.24% sys:21.94% idle:76.60% iow: 0.00% irq:
  0.20% sirq: 0.00%
  client:   net CPU 81: usr: 0.00% sys: 0.00% idle:54.74% iow: 0.00% irq:
  2.82% sirq:42.42%
  client:   app CPU 82: usr: 1.24% sys:22.56% idle:75.77% iow: 0.00% irq:
  0.20% sirq: 0.20%
  client: == Target
  client:   Tx  0.000 Gbps (0 bytes in 5281143 usec)
  client:   Tx  0.000 Gbps (0 bytes in 5281143 usec)
  client:   Rx 89.159 Gbps (58857881600 bytes in 5281143 usec)
  client:   Rx 89.457 Gbps (59054489600 bytes in 5281143 usec)
  client:   TCP retrans reord rtt rttvar d_ce snd_wnd cwnd
  client:             0     0 121     55    0   63488   10
  client:             0     0 113     60    0   63488   10
  client:   net CPU 81: usr: 0.00% sys: 0.00% idle:47.80% iow: 0.00% irq:
  1.90% sirq:50.28%
  client:   app CPU 82: usr: 2.51% sys:85.46% idle:11.43% iow: 0.00% irq:
  0.38% sirq: 0.19%

worker.c
@@ -93,6 +93,8 @@ worker_kill_conn(struct worker_state *self, struct connection *conn)
 		warn("Failed to del poll out");
 	if (self->rx_mode == KPM_RX_MODE_DEVMEM)
 		(void)devmem_release_tokens(conn->fd, &conn->devmem);
+	if (self->tx_mode == KPM_TX_MODE_DEVMEM)
+		devmem_teardown_conn(&conn->devmem);
 	close(conn->fd);
 	list_del(&conn->connections);
 	free(conn->rxbuf);
@@ -329,6 +331,13 @@ worker_msg_test(struct worker_state *self, struct kpm_header *hdr)
 			warnx("Failed to set SO_ZEROCOPY");
 			self->quit = 1;
 			return;
+		}
+
+		if (self->tx_mode == KPM_TX_MODE_DEVMEM) {
+			if (devmem_setup_conn(conn->fd, &conn->devmem) < 0) {
+				self->quit = 1;
+				return;
+			}
 		}
 
 		ev.events = EPOLLIN | EPOLLOUT;
@@ -579,12 +588,19 @@ worker_handle_send(struct worker_state *self, struct connection *conn,
 	int flags = msg_zerocopy ? MSG_ZEROCOPY : 0;
 
 	while (rep--) {
-		void *src = &patbuf[conn->tot_sent % PATTERN_PERIOD];
 		size_t chunk;
+		void *src;
 		ssize_t n;
 
 		chunk = min_t(size_t, conn->write_size, conn->to_send);
-		n = send(conn->fd, src, chunk, MSG_DONTWAIT | flags);
+
+		if (self->tx_mode == KPM_TX_MODE_DEVMEM) {
+			n = devmem_sendmsg(conn->fd, &conn->devmem,
+					   conn->tot_sent % PATTERN_PERIOD, chunk);
+		} else {
+			src = &patbuf[conn->tot_sent % PATTERN_PERIOD];
+			n = send(conn->fd, src, chunk, MSG_DONTWAIT | flags);
+		}
 		if (n == 0) {
 			warnx("zero send chunk:%zd to_send:%lld to_recv:%lld",
 			      chunk, conn->to_send, conn->to_recv);
@@ -604,7 +620,7 @@ worker_handle_send(struct worker_state *self, struct connection *conn,
 
 		conn->to_send -= n;
 		conn->tot_sent += n;
-		if (msg_zerocopy) {
+		if (msg_zerocopy || self->tx_mode == KPM_TX_MODE_DEVMEM) {
 			conn->to_send_comp += 1;
 			kpm_dbg("queued send completion, total %d",
 				conn->to_send_comp);

client.c
@@ -24,6 +24,7 @@ int verbose = 3;
 static struct {
 	bool msg_trunc;
 	bool devmem_rx;
+	bool devmem_tx;
 	bool msg_zerocopy;
 	bool tls;
 	bool tls_rx;
@@ -147,6 +148,7 @@ static const struct opt_table opts[] = {
 	OPT_WITHOUT_ARG("--msg-trunc", opt_set_bool, &opt.msg_trunc, "Use MSG_TRUNC on receive"),
 	OPT_WITHOUT_ARG("--msg-zerocopy", opt_set_bool, &opt.msg_zerocopy, "Use MSG_ZEROCOPY on transmit"),
 	OPT_EARLY_WITHOUT_ARG("--devmem-rx", opt_set_bool, &opt.devmem_rx, "Use TCP Devmem on receive"),
+	OPT_WITHOUT_ARG("--devmem-tx", opt_set_bool, &opt.devmem_tx, "Use TCP Devmem on transmit"),
 	OPT_WITH_ARG("--udmabuf-size-mb <arg>", opt_set_uintval, opt_show_uintval,
 		     &opt.udmabuf_size_mb, "Size of RX udmabuf for TCP Devmem mode"),
 	OPT_WITH_ARG("--num-rx-queues <arg>", opt_set_uintval, opt_show_uintval,
@@ -640,8 +642,13 @@ int main(int argc, char *argv[])
 	else if (opt.devmem_rx)
 		rx_mode = KPM_RX_MODE_DEVMEM;
 
+	if (opt.msg_zerocopy && opt.devmem_tx)
+		errx(1, "--msg-zerocopy and --devmem-tx are mutually exclusive");
+
 	if (opt.msg_zerocopy)
 		tx_mode = KPM_TX_MODE_SOCKET_ZEROCOPY;
+	else if (opt.devmem_tx)
+		tx_mode = KPM_TX_MODE_DEVMEM;
 
 	if (kpm_req_mode(dst, rx_mode, tx_mode, opt.udmabuf_size_mb,
 			 opt.num_rx_queues, opt.validate) < 0) {

devmem.c
@@ -29,6 +29,8 @@
 
 #include "server.h"
 
+#define ROUND_UP(n, d) (((n) + (d) - 1) / (d))
+
 extern unsigned char patbuf[KPM_MAX_OP_CHUNK + PATTERN_PERIOD + 1];
 
 static int ethtool(const char *ifname, void *data)
@@ -292,6 +294,42 @@ out:
 	return ret;
 }
 
+static int bind_tx_queue(unsigned int ifindex, unsigned int dmabuf_fd,
+			 struct ynl_sock *ys)
+{
+	struct netdev_bind_tx_req *req = NULL;
+	struct netdev_bind_tx_rsp *rsp = NULL;
+	int ret;
+
+	req = netdev_bind_tx_req_alloc();
+	netdev_bind_tx_req_set_ifindex(req, ifindex);
+	netdev_bind_tx_req_set_fd(req, dmabuf_fd);
+
+	rsp = netdev_bind_tx(ys, req);
+	if (!rsp) {
+		warnx("netdev_bind_tx");
+		ret = -1;
+		goto err_close;
+	}
+
+	if (!rsp->_present.id) {
+		warnx("id not present");
+		ret = -1;
+		goto err_close;
+	}
+
+	ret = rsp->id;
+	netdev_bind_tx_req_free(req);
+	netdev_bind_tx_rsp_free(rsp);
+
+	return ret;
+
+err_close:
+	netdev_bind_tx_req_free(req);
+	ynl_sock_destroy(ys);
+	return ret;
+}
+
 #define UDMABUF_LIMIT_PATH "/sys/module/udmabuf/parameters/size_limit_mb"
 
 static int udmabuf_check_size(size_t size_mb)
@@ -433,6 +471,21 @@ static int find_iface(struct sockaddr_in6 *addr, char ifname[IFNAMSIZ])
 	return -ENODEV;
 }
 
+void udmabuf_memcpy_to_device(struct memory_buffer *dst, size_t off,
+			      void *src, int n)
+{
+	struct dma_buf_sync sync = {};
+
+	sync.flags = DMA_BUF_SYNC_START | DMA_BUF_SYNC_WRITE;
+	ioctl(dst->fd, DMA_BUF_IOCTL_SYNC, &sync);
+
+	memcpy(dst->buf_mem + off, src, n);
+
+	sync.flags = DMA_BUF_SYNC_END | DMA_BUF_SYNC_WRITE;
+	ioctl(dst->fd, DMA_BUF_IOCTL_SYNC, &sync);
+}
+
+/* Setup Devmem RX */
 int devmem_setup(struct session_state_devmem *devmem, int fd,
 		 size_t udmabuf_size_mb, int num_queues)
 {
@@ -691,4 +744,102 @@ ssize_t devmem_recv(int fd, struct connection_devmem *conn,
 	}
 
 	return n;
+}
+
+int devmem_sendmsg(int fd, struct connection_devmem *devmem, size_t off, size_t n)
+{
+	char ctrl_data[CMSG_SPACE(sizeof(int))];
+	struct msghdr msg = { 0 };
+	struct cmsghdr *cmsg;
+	struct iovec iov;
+
+	iov.iov_base = (void *)off;
+	iov.iov_len = n;
+
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+
+	msg.msg_control = ctrl_data;
+	msg.msg_controllen = sizeof(ctrl_data);
+
+	cmsg = CMSG_FIRSTHDR(&msg);
+	cmsg->cmsg_level = SOL_SOCKET;
+	cmsg->cmsg_type = SCM_DEVMEM_DMABUF;
+	cmsg->cmsg_len = CMSG_LEN(sizeof(int));
+	*((int *)CMSG_DATA(cmsg)) = devmem->mem.dmabuf_id;
+
+	return sendmsg(fd, &msg, MSG_SOCK_DEVMEM);
+}
+
+/* Setup Devmem TX */
+int devmem_setup_conn(int fd, struct connection_devmem *devmem)
+{
+	char ifname[IFNAMSIZ] = {};
+	struct sockaddr_in6 addr;
+	struct ynl_error yerr;
+	socklen_t optlen;
+	int ifindex;
+	int opt = 1;
+	int ret;
+
+	devmem->ys = ynl_sock_create(&ynl_netdev_family, &yerr);
+	if (!devmem->ys) {
+		warnx("Failed to setup YNL socket: %s", yerr.msg);
+		return -1;
+	}
+
+	optlen = sizeof(addr);
+	if (getsockname(fd, (struct sockaddr *)&addr, &optlen) < 0) {
+		warn("Failed to query socket address");
+		return -1;
+	}
+
+	if (addr.sin6_family == AF_INET)
+		inet_to_inet6((void *)&addr, &addr);
+
+	ifindex = find_iface(&addr, ifname);
+	if (ifindex < 0) {
+		warnx("Failed to resolve ifindex: %s", strerror(-ifindex));
+		return -1;
+	}
+
+	if (udmabuf_alloc(&devmem->mem, "udmabuf-test-tx",
+			  ROUND_UP(sizeof(patbuf), 1024 * 1024)) < 0) {
+		warnx("Failed to allocate devmem tx buffer");
+		return -1;
+	}
+
+	devmem->mem.dmabuf_id = bind_tx_queue(ifindex, devmem->mem.fd,
+					      devmem->ys);
+	if (devmem->mem.dmabuf_id < 0) {
+		ret = -1;
+		goto free_udmabuf;
+	}
+
+	udmabuf_memcpy_to_device(&devmem->mem, 0, patbuf, sizeof(patbuf));
+
+	if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, ifname, IFNAMSIZ)) {
+		warn("failed to bind device to socket");
+		ret = -1;
+		goto free_udmabuf;
+	}
+
+	if (setsockopt(fd, SOL_SOCKET, SO_ZEROCOPY, &opt, sizeof(opt))) {
+		warnx("failed to set SO_ZEROCOPY");
+		ret = -1;
+		goto free_udmabuf;
+	}
+
+	return 0;
+
+free_udmabuf:
+	udmabuf_free(&devmem->mem);
+	return ret;
+}
+
+void devmem_teardown_conn(struct connection_devmem *devmem)
+{
+	udmabuf_free(&devmem->mem);
+	ynl_sock_destroy(devmem->ys);
+	devmem->ys = NULL;
 }

proto.h
@@ -131,6 +131,7 @@ enum kpm_rx_mode {
 enum kpm_tx_mode {
 	KPM_TX_MODE_SOCKET,
 	KPM_TX_MODE_SOCKET_ZEROCOPY,
+	KPM_TX_MODE_DEVMEM,
 };
 
 struct kpm_mode {

server.h
@@ -58,6 +58,8 @@ struct connection_devmem {
 	int rxtok_len;
 	/* ncdevmem uses 80k, allocate 64k for recvmsg tokens */
 	char ctrl_data[64 * 1024];
+	struct memory_buffer mem;
+	struct ynl_sock *ys;
 };
 
 struct session_state_devmem {
@@ -84,5 +86,8 @@ int devmem_release_tokens(int fd, struct connection_devmem *conn);
 ssize_t devmem_recv(int fd, struct connection_devmem *conn,
 		    unsigned char *rxbuf, size_t chunk, struct memory_buffer *mem,
 		    int rep, __u64 tot_recv, bool validate);
+int devmem_sendmsg(int fd, struct connection_devmem *devmem, size_t off, size_t n);
+int devmem_setup_conn(int fd, struct connection_devmem *devmem);
+void devmem_teardown_conn(struct connection_devmem *devmem);
 
 #endif /* SERVER_H */

commit 230d81055a3a974fa9ad6bc131bedfcd2e7febd5

devmem: fix SO_ZEROCOPY for devmem TX
Add devmem TX to the modes that cause the worker to set SO_ZEROCOPY.

This way, devmem TX is more like non-devmem modes, and does not rely on
devmem_setup_* calls to set the option.

Fixes: 5b58bf3ca7ad ("devmem: add support for devmem TX")

worker.c
@@ -326,7 +326,7 @@ worker_msg_test(struct worker_state *self, struct kpm_header *hdr)
 		else
 			conn->to_recv = len;
 
-		zc = self->tx_mode == KPM_TX_MODE_SOCKET_ZEROCOPY;
+		zc = self->tx_mode == KPM_TX_MODE_SOCKET_ZEROCOPY || self->tx_mode == KPM_TX_MODE_DEVMEM;
 		if (setsockopt(conn->fd, SOL_SOCKET, SO_ZEROCOPY, &zc, sizeof(zc))) {
 			warnx("Failed to set SO_ZEROCOPY");
 			self->quit = 1;