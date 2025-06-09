beshleman/kperf

commit 66ba1fad2d5c56aa10e9475acf551ba3be11140a

devmem: support using the CUDA memory provider for devmem RX
Add the ability to select the CUDA memory provider for
devmem RX.

Using args:
--devmem-rx --devmem-rx-memory cuda
--devmem-dst-dev '0000:5f:00'

Signed-off-by: Bobby Eshleman <bobbyeshleman@meta.com>

client.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /* Copyright Meta Platforms, Inc. and affiliates */
 
+#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
@@ -24,6 +25,8 @@ int verbose = 3;
 static struct {
 	bool msg_trunc;
 	bool devmem_rx;
+	enum memory_provider_type devmem_rx_memory;
+	struct pci_dev devmem_dst_dev;
 	bool msg_zerocopy;
 	bool tls;
 	bool tls_rx;
@@ -73,6 +76,12 @@ static struct {
 	/* 128M is enough to drive one queue at 200G */
 	.udmabuf_size_mb = 128,
 	.num_rx_queues = 1,
+	.devmem_rx_memory = MEMORY_PROVIDER_HOST,
+	.devmem_dst_dev = {
+		.domain = DEVICE_DOMAIN_ANY,
+		.bus = DEVICE_BUS_ANY,
+		.device = DEVICE_DEVICE_ANY
+	},
 };
 
 #define dbg(fmt...) while (0) { warnx(fmt); }
@@ -80,6 +89,88 @@ static struct {
 static void opt_show_uinthex(char buf[OPT_SHOW_LEN], const unsigned int *ui)
 {
 	sprintf(buf, "0x%x", *ui);
+}
+
+static char *arg_bad(const char *fmt, const char *arg)
+{
+	char *str;
+
+	str = malloc(strlen(fmt) + strlen(arg));
+	if (!str)
+		return strerror(errno);
+
+	sprintf(str, fmt, arg);
+
+	return str;
+}
+
+static char *
+opt_set_memory_provider(const char *arg, enum memory_provider_type *provider)
+{
+	char *ret;
+
+	if (!strcmp(arg, "cuda")) {
+#ifndef USE_CUDA
+		return arg_bad("memory provider %s requires kperf compiled with CUDA", arg);
+#endif
+	}
+
+	ret = NULL;
+	if (!strcmp(arg, "host")) {
+		*provider = MEMORY_PROVIDER_HOST;
+	} else if (!strcmp(arg, "cuda")) {
+		*provider = MEMORY_PROVIDER_CUDA;
+	} else {
+		ret = arg_bad("'%s' is not a valid memory provider", arg);
+	}
+
+	return ret;
+
+}
+
+static char *
+opt_set_dev(const char *arg, struct pci_dev *dev)
+{
+	if (!strcmp(arg, "any")) {
+		dev->domain = DEVICE_DOMAIN_ANY;
+		dev->bus = DEVICE_BUS_ANY;
+		dev->device = DEVICE_DEVICE_ANY;
+		return NULL;
+	}
+
+	if (sscanf(arg, "%hx:%hhx:%hhx", &dev->domain, &dev->bus, &dev->device) == 3)
+		return NULL;
+
+	return arg_bad("'%s' invalid PCI ID format. Expected format: domain:bus:device\n", arg);
+}
+
+static void
+opt_show_memory_provider(char buf[OPT_SHOW_LEN], const enum memory_provider_type *p)
+{
+	switch (*p) {
+	case MEMORY_PROVIDER_HOST:
+		strncpy(buf, "host", OPT_SHOW_LEN);
+		break;
+	case MEMORY_PROVIDER_CUDA:
+		strncpy(buf, "cuda", OPT_SHOW_LEN);
+		break;
+	default:
+		/* inval */
+		strncpy(buf, "invalid", OPT_SHOW_LEN);
+		break;
+	}
+}
+
+static void
+opt_show_dev(char buf[OPT_SHOW_LEN], const struct pci_dev *dev)
+{
+	if (dev->domain == DEVICE_DOMAIN_ANY &&
+	    dev->bus == DEVICE_BUS_ANY &&
+	    dev->device == DEVICE_DEVICE_ANY)
+		strncpy(buf, "any", OPT_SHOW_LEN);
+	else
+		snprintf(buf, OPT_SHOW_LEN, "%hx:%hhx:%hhx",
+			 dev->domain, dev->bus, dev->device);
 }
 
 static const struct opt_table opts[] = {
@@ -147,12 +238,17 @@ static const struct opt_table opts[] = {
 	OPT_WITHOUT_ARG("--msg-trunc", opt_set_bool, &opt.msg_trunc, "Use MSG_TRUNC on receive"),
 	OPT_WITHOUT_ARG("--msg-zerocopy", opt_set_bool, &opt.msg_zerocopy, "Use MSG_ZEROCOPY on transmit"),
 	OPT_EARLY_WITHOUT_ARG("--devmem-rx", opt_set_bool, &opt.devmem_rx, "Use TCP Devmem on receive"),
-	OPT_WITH_ARG("--udmabuf-size-mb <arg>", opt_set_uintval, opt_show_uintval,
-		     &opt.udmabuf_size_mb, "Size of RX udmabuf for TCP Devmem mode"),
+	OPT_WITH_ARG("--devmem-rx-memory {cuda,host}", opt_set_memory_provider,
+		     opt_show_memory_provider, &opt.devmem_rx_memory,
+		     "Select the memory provider for TCP Devmem RX"),
+	OPT_WITH_ARG("--dmabuf-size-mb <arg>", opt_set_uintval, opt_show_uintval,
+		     &opt.udmabuf_size_mb, "Size of RX dmabuf for TCP Devmem mode"),
 	OPT_WITH_ARG("--num-rx-queues <arg>", opt_set_uintval, opt_show_uintval,
 		     &opt.num_rx_queues, "Number of RX queues for TCP Devmem mode"),
 	OPT_WITH_ARG("--validate <yes|no>", opt_set_bool_arg, NULL, &opt.validate,
 		     "Validate payload. Default is no when using --devmem-rx; otherwise, default is yes"),
+	OPT_WITH_ARG("--devmem-dst-dev <arg>", opt_set_dev, opt_show_dev,
+		     &opt.devmem_dst_dev, "Select the destination device for the TCP Devmem memory provider"),
 	OPT_ENDTABLE
 };
 
@@ -556,6 +652,7 @@ int main(int argc, char *argv[])
 	__u32 *src_wrk_id, *dst_wrk_id;
 	struct sockaddr_in6 conn_addr;
 	__u32 src_tst_id, dst_tst_id;
+	struct pci_dev dst_dev;
 	struct addrinfo *addr;
 	struct kpm_test *test;
 	unsigned int i;
@@ -640,17 +737,23 @@ int main(int argc, char *argv[])
 	else if (opt.devmem_rx)
 		rx_mode = KPM_RX_MODE_DEVMEM;
 
+	/* TODO: support --validate yes for cuda rx */
+	if (opt.validate && opt.devmem_rx_memory == MEMORY_PROVIDER_CUDA)
+		errx(1, "--devmem-rx-memory cuda does not support --validate yes");
+
 	if (opt.msg_zerocopy)
 		tx_mode = KPM_TX_MODE_SOCKET_ZEROCOPY;
 
 	if (kpm_req_mode(dst, rx_mode, tx_mode, opt.udmabuf_size_mb,
-			 opt.num_rx_queues, opt.validate) < 0) {
+			 opt.num_rx_queues, opt.validate,
+			 opt.devmem_rx_memory, &dst_dev) < 0) {
 		warnx("Failed setup destination mode");
 		goto out;
 	}
 
-	if (kpm_req_mode(src, rx_mode, tx_mode, opt.udmabuf_size_mb, opt.num_rx_queues,
-			 opt.validate) < 0) {
+	if (kpm_req_mode(src, rx_mode, tx_mode, opt.udmabuf_size_mb,
+			 opt.num_rx_queues, opt.validate,
+			 opt.devmem_rx_memory, &dst_dev) < 0) {
 		warnx("Failed setup source mode");
 		goto out;
 	}

devmem.c
@@ -650,7 +650,9 @@ static struct memory_provider *get_memory_provider(enum memory_provider_type pro
 
 /* Setup Devmem RX */
 int devmem_setup(struct session_state_devmem *devmem, int fd,
-		 size_t udmabuf_size_mb, int num_queues)
+		 size_t udmabuf_size_mb, int num_queues,
+		 enum memory_provider_type provider,
+		 struct pci_dev *dev)
 {
 	struct netdev_queue_id *queues;
 	char ifname[IFNAMSIZ] = {};
@@ -672,6 +674,10 @@ int devmem_setup(struct session_state_devmem *devmem, int fd,
 		warn("Failed to query socket address");
 		return -1;
 	}
+
+	rxmp = get_memory_provider(provider);
+	if (!rxmp)
+		return -1;
 
 	if (addr.sin6_family == AF_INET)
 		inet_to_inet6((void *)&addr, &addr);
@@ -695,9 +701,14 @@ int devmem_setup(struct session_state_devmem *devmem, int fd,
 		goto sock_destroy;
 	}
 
+	if (rxmp->dev_init && rxmp->dev_init(dev) < 0) {
+		ret = -1;
+		goto sock_destroy;
+	}
+
 	devmem->mem = rxmp->alloc(udmabuf_size_mb * 1024 * 1024);
 	if (!devmem->mem) {
-		warnx("Failed to allocate udmabuf");
+		warnx("Failed to allocate memory");
 		ret = -1;
 		goto sock_destroy;
 	}
@@ -706,7 +717,7 @@ int devmem_setup(struct session_state_devmem *devmem, int fd,
 		warnx("Invalid number of RX queues (%u) requested (max: %u)",
 		      num_queues, rxqn - 1);
 		ret = -1;
-		goto free_udmabuf;
+		goto free_memory;
 	}
 
 	max_kernel_queue = rxqn - num_queues;
@@ -715,7 +726,7 @@ int devmem_setup(struct session_state_devmem *devmem, int fd,
 	if (rss_equal(ifname, max_kernel_queue)) {
 		warnx("Failed to setup RSS");
 		ret = -1;
-		goto free_udmabuf;
+		goto free_memory;
 	}
 
 	memcpy(devmem->ifname, ifname, IFNAMSIZ);
@@ -756,7 +767,7 @@ undo_rss_context:
 	rss_context_delete(devmem);
 undo_rss:
 	rss_equal(ifname, rxqn);
-free_udmabuf:
+free_memory:
 	rxmp->free(devmem->mem);
 sock_destroy:
 	ynl_sock_destroy(devmem->ys);
@@ -780,7 +791,8 @@ int devmem_teardown(struct session_state_devmem *devmem)
 	}
 	if (devmem->ys)
 		ynl_sock_destroy(devmem->ys);
-	rxmp->free(devmem->mem);
+	if (rxmp)
+		rxmp->free(devmem->mem);
 	return 0;
 }
 
@@ -822,6 +834,7 @@ static int devmem_validate_token(struct memory_buffer *mem,
 	ioctl(mem->fd, DMA_BUF_IOCTL_SYNC, &sync);
 
 	pat = &patbuf[start];
+	/* TODO: memory_provider for CUDA case? */
 	ret = memcmp(pat, mem->buf_mem + dmabuf_cmsg->frag_offset, dmabuf_cmsg->frag_size);
 
 	sync.flags = DMA_BUF_SYNC_END;

proto.c
@@ -181,7 +181,9 @@ int kpm_send_tcp_cc(int fd, __u32 id, char *cc_name)
 }
 
 int kpm_send_mode(int fd, enum kpm_rx_mode rx_mode, enum kpm_tx_mode tx_mode,
-		  __u32 udmabuf_size_mb, __u32 num_rx_queues, __u8 validate)
+		  __u32 udmabuf_size_mb, __u32 num_rx_queues, __u8 validate,
+		  enum memory_provider_type rx_provider,
+		  struct pci_dev *dev)
 {
 	struct kpm_mode msg = {};
 
@@ -190,6 +192,9 @@ int kpm_send_mode(int fd, enum kpm_rx_mode rx_mode, enum kpm_tx_mode tx_mode,
 	msg.udmabuf_size_mb = udmabuf_size_mb;
 	msg.num_rx_queues = num_rx_queues;
 	msg.validate = validate;
+	msg.rx_provider = rx_provider;
+
+	memcpy(&msg.dev, dev, sizeof(msg.dev));
 
 	return kpm_send(fd, &msg.hdr, sizeof(msg), KPM_MSG_TYPE_MODE);
 }
@@ -459,12 +464,15 @@ kpm_req_tcp_cc(int fd, __u32 conn_id, char *cc_name)
 
 int
 kpm_req_mode(int fd, enum kpm_rx_mode rx_mode, enum kpm_tx_mode tx_mode,
-	     __u32 udmabuf_size_mb, __u32 num_rx_queues, __u8 validate)
+	     __u32 udmabuf_size_mb, __u32 num_rx_queues, __u8 validate,
+	     enum memory_provider_type rx_provider,
+	     struct pci_dev *dev)
 {
 	struct kpm_empty *repl;
 	int id;
 
-	id = kpm_send_mode(fd, rx_mode, tx_mode, udmabuf_size_mb, num_rx_queues, validate);
+	id = kpm_send_mode(fd, rx_mode, tx_mode, udmabuf_size_mb, num_rx_queues, validate,
+			   rx_provider, dev);
 	if (id < 0) {
 		warnx("Failed to request mode");
 		return id;

proto.h
@@ -133,6 +133,11 @@ enum kpm_tx_mode {
 	KPM_TX_MODE_SOCKET_ZEROCOPY,
 };
 
+enum memory_provider_type {
+	MEMORY_PROVIDER_HOST,
+	MEMORY_PROVIDER_CUDA,
+};
+
 #define DEVICE_DOMAIN_ANY 0xffff
 #define DEVICE_BUS_ANY 0xff
 #define DEVICE_DEVICE_ANY 0xff
@@ -147,8 +152,13 @@ struct kpm_mode {
 	struct kpm_header hdr;
 	enum kpm_rx_mode rx_mode;
 	enum kpm_tx_mode tx_mode;
+
+	/* devmem info */
+	enum memory_provider_type rx_provider;
+	struct pci_dev dev;
 	__u32 udmabuf_size_mb;
 	__u32 num_rx_queues;
+
 	__u8 validate;
 };
 
@@ -284,7 +294,9 @@ int kpm_send_tls(int fd, __u32 conn_id, __u32 dir_mask,
 int kpm_send_max_pacing(int fd, __u32 id, __u32 max_pace);
 int kpm_send_tcp_cc(int fd, __u32 id, char *cc_name);
 int kpm_send_mode(int fd, enum kpm_rx_mode rx_mode, enum kpm_tx_mode tx_mode,
-		  __u32 udmabuf_size_mb, __u32 num_rx_queues, __u8 validate);
+		  __u32 udmabuf_size_mb, __u32 num_rx_queues, __u8 validate,
+		  enum memory_provider_type rx_provider,
+		  struct pci_dev *dev);
 int kpm_send_pin_worker(int fd, __u32 id, __u32 cpu);
 
 void kpm_reply_error(int fd, struct kpm_header *hdr, __u16 error);
@@ -308,7 +320,9 @@ int kpm_req_tls(int fd, __u32 conn_id, __u32 dir_mask,
 int kpm_req_pacing(int fd, __u32 conn_id, __u32 max_pace);
 int kpm_req_tcp_cc(int fd, __u32 conn_id, char *cc_name);
 int kpm_req_mode(int fd, enum kpm_rx_mode rx_mode, enum kpm_tx_mode tx_mode,
-		 __u32 udmabuf_size_mb, __u32 num_rx_queues, __u8 validate);
+		 __u32 udmabuf_size_mb, __u32 num_rx_queues, __u8 validate,
+		 enum memory_provider_type rx_provider,
+		 struct pci_dev *dev);
 int kpm_req_disconnect(int fd, __u32 connection_id);
 
 #endif /* PROTO_H */

server.h
@@ -15,6 +15,10 @@
 
 #include "proto.h"
 
+#ifdef USE_CUDA
+#include <cuda.h>
+#endif
+
 #define PATTERN_PERIOD 255
 
 struct server_session {
@@ -43,6 +47,12 @@ struct dmabuf_token {
 };
 #endif
 
+#ifdef USE_CUDA
+struct memory_buffer_cuda {
+	CUcontext ctx;
+};
+#endif
+
 struct memory_buffer {
 	char *buf_mem;
 	size_t size;
@@ -50,6 +60,10 @@ struct memory_buffer {
 	int devfd;
 	int memfd;
 	int dmabuf_id;
+	void *priv;
+#ifdef USE_CUDA
+	struct memory_buffer_cuda cuda;
+#endif
 };
 
 struct memory_provider {
@@ -87,7 +101,8 @@ void NORETURN pworker_main(int fd, enum kpm_rx_mode rx_mode, enum kpm_tx_mode tx
                            struct memory_buffer *devmem, bool validate);
 
 int devmem_setup(struct session_state_devmem *devmem, int fd,
-		 size_t udmabuf_size, int num_queues);
+		 size_t udmabuf_size, int num_queues,
+		 enum memory_provider_type provider, struct pci_dev *dev);
 int devmem_teardown(struct session_state_devmem *devmem);
 int devmem_release_tokens(int fd, struct connection_devmem *conn);
 ssize_t devmem_recv(int fd, struct connection_devmem *conn,

server_session.c
@@ -513,7 +513,8 @@ server_msg_mode(struct session_state *self, struct kpm_header *hdr)
 
 	if (self->tcp_sock && req->rx_mode == KPM_RX_MODE_DEVMEM) {
 		ret = devmem_setup(&self->devmem, self->tcp_sock, req->udmabuf_size_mb,
-				   req->num_rx_queues);
+				   req->num_rx_queues, req->rx_provider,
+				   &req->dev);
 		if (ret < 0) {
 			warnx("Failed to setup devmem");
 			self->quit = 1;

commit 9d69a6a466bd43c9b0ccdc3ac677e1d3b7514b05

client: use opt.devmem_dst_dev instead of stack-allocated dst_dev
Pass the destination device initialized from options instead of the
uninitialized stack allocated one. Remove the one from the stack
since it isn't needed anymore.

Fixes: 66ba1fad2d5c ("devmem: support using the CUDA memory provider for
devmem RX")

client.c
@@ -652,7 +652,6 @@ int main(int argc, char *argv[])
 	__u32 *src_wrk_id, *dst_wrk_id;
 	struct sockaddr_in6 conn_addr;
 	__u32 src_tst_id, dst_tst_id;
-	struct pci_dev dst_dev;
 	struct addrinfo *addr;
 	struct kpm_test *test;
 	unsigned int i;
@@ -746,14 +745,14 @@ int main(int argc, char *argv[])
 
 	if (kpm_req_mode(dst, rx_mode, tx_mode, opt.dmabuf_size_mb,
 			 opt.num_rx_queues, opt.validate,
-			 opt.devmem_rx_memory, &dst_dev) < 0) {
+			 opt.devmem_rx_memory, &opt.devmem_dst_dev) < 0) {
 		warnx("Failed setup destination mode");
 		goto out;
 	}
 
 	if (kpm_req_mode(src, rx_mode, tx_mode, opt.dmabuf_size_mb,
 			 opt.num_rx_queues, opt.validate,
-			 opt.devmem_rx_memory, &dst_dev) < 0) {
+			 opt.devmem_rx_memory, &opt.devmem_dst_dev) < 0) {
 		warnx("Failed setup source mode");
 		goto out;
 	}