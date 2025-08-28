beshleman/kperf

commit 3b4a31b4b8f28e4d2ca2bd3fce561bbbb956c775

worker: refactor worker_main_args to worker_opts
Rename worker_main_args to worker_opts and add directly to worker_state.
This reduces the amount of churn esp when adding new opts.

Signed-off-by: David Wei <dw@davidwei.uk>

epoll.c
@@ -240,7 +240,7 @@ ep_handle_send(struct worker_state *self, struct worker_connection *conn,
 		chunk = min_t(size_t, conn->write_size, conn->to_send);
 
 		if (self->tx_mode == KPM_TX_MODE_DEVMEM) {
-			n = devmem_sendmsg(conn->fd, self->devmem.dmabuf_id,
+			n = devmem_sendmsg(conn->fd, self->opts.devmem.dmabuf_id,
 					   conn->tot_sent % PATTERN_PERIOD, chunk);
 		} else {
 			src = &patbuf[conn->tot_sent % PATTERN_PERIOD];
@@ -367,7 +367,7 @@ ep_handle_recv(struct worker_state *self, struct worker_connection *conn)
 		chunk = min_t(size_t, conn->read_size, conn->to_recv);
 		if (self->rx_mode == KPM_RX_MODE_DEVMEM)
 			n = devmem_recv(conn->fd, &conn->devmem,
-					conn->rxbuf, chunk, self->devmem.mem,
+					conn->rxbuf, chunk, self->opts.devmem.mem,
 					rep, conn->tot_recv, self->validate);
 		else if (self->rx_mode == KPM_RX_MODE_SOCKET_ZEROCOPY)
 			n = ep_handle_zerocopy_recv(self, conn, chunk, rep);

iou.c
@@ -305,7 +305,7 @@ static int iou_register_zerocopy_rx(struct worker_state *self)
 	void *ring_ptr;
 	int ret;
 
-	area_size = self->iou.rx_size_mb * 1024 * 1024;
+	area_size = self->opts.iou.rx_size_mb * 1024 * 1024;
 	/* arbitrary ring size chosen based on rx_size_mb */
 	ring_entries = (area_size / (page_size * 2));
 	ring_size = get_rq_ring_size(ring_entries);
@@ -335,8 +335,8 @@ static int iou_register_zerocopy_rx(struct worker_state *self)
 	};
 
 	struct io_uring_zcrx_ifq_reg reg = {
-		.if_idx = self->iou.ifindex,
-		.if_rxq = self->iou.queue_id,
+		.if_idx = self->opts.iou.ifindex,
+		.if_rxq = self->opts.iou.queue_id,
 		.rq_entries = ring_entries,
 		.area_ptr = (__u64)(unsigned long)&area_reg,
 		.region_ptr = (__u64)(unsigned long)&region_reg,

worker.h
@@ -15,6 +15,7 @@
 /* Main worker state AKA self */
 struct worker_state {
 	int main_sock;
+	struct worker_opts opts;
 	enum kpm_rx_mode rx_mode;
 	enum kpm_tx_mode tx_mode;
 	int epollfd;
@@ -27,8 +28,6 @@ struct worker_state {
 	struct timemono prev_loop;
 	unsigned int test_len_msec;
 	struct list_head connections;
-	struct worker_state_devmem devmem;
-	struct worker_state_iou iou;
 	bool validate;
 	const struct io_ops *ops;
 	void *io_state;

server.h
@@ -119,22 +119,18 @@ struct worker_state_iou {
 	int queue_id;
 };
 
-struct worker_main_args {
-	int fd;
+struct worker_opts {
 	enum kpm_rx_mode rx_mode;
 	enum kpm_tx_mode tx_mode;
-	struct memory_buffer *devmem;
 	bool validate;
-	int dmabuf_id;
-	bool iou;
-	unsigned int iou_rx_size_mb;
-	int ifindex;
-	int queue_id;
+	bool use_iou;
+	struct worker_state_devmem devmem;
+	struct worker_state_iou iou;
 };
 
 struct server_session *
 server_session_spawn(int fd, struct sockaddr_in6 *addr, socklen_t *addrlen);
 
-void NORETURN pworker_main(struct worker_main_args args);
+void NORETURN pworker_main(int fd, struct worker_opts opts);
 
 #endif /* SERVER_H */

server_session.c
@@ -593,19 +593,22 @@ server_msg_spawn_pworker(struct session_state *self, struct kpm_header *hdr)
 		int dmabuf_id = self->devmem.tx_mem ? self->devmem.tx_mem->dmabuf_id : -1;
 
 		close(p[0]);
-		struct worker_main_args args = {
-			.fd = p[1],
+		struct worker_opts opts = {
 			.rx_mode = self->rx_mode,
 			.tx_mode = self->tx_mode,
-			.devmem = self->devmem.mem,
 			.validate = self->validate,
-			.dmabuf_id = dmabuf_id,
-			.iou = self->iou,
-			.iou_rx_size_mb = self->iou_state.rx_size_mb,
-			.ifindex = self->iou_state.ifindex,
-			.queue_id = self->iou_state.queue_id,
+			.use_iou = self->iou,
+			.devmem = {
+				.mem = self->devmem.mem,
+				.dmabuf_id = dmabuf_id,
+			},
+			.iou = {
+				.rx_size_mb = self->iou_state.rx_size_mb,
+				.ifindex = self->iou_state.ifindex,
+				.queue_id = self->iou_state.queue_id,
+			},
 		};
-		pworker_main(args);
+		pworker_main(p[1], opts);
 		exit(1);
 	}
 

worker.c
@@ -389,22 +389,14 @@ worker_recv_finished(struct worker_state *self, struct worker_connection *conn)
 
 /* == Main loop == */
 
-void NORETURN pworker_main(struct worker_main_args args)
+void NORETURN pworker_main(int fd, struct worker_opts opts)
 {
 	struct worker_state self = {
-		.main_sock = args.fd,
-		.rx_mode = args.rx_mode,
-		.tx_mode = args.tx_mode,
-		.validate = args.validate,
-		.devmem = { .mem = args.devmem, .dmabuf_id = args.dmabuf_id },
-		.iou = {
-			.rx_size_mb = args.iou_rx_size_mb,
-			.ifindex = args.ifindex,
-			.queue_id = args.queue_id,
-		},
+		.main_sock = fd,
+		.opts = opts,
 	};
 
-	if (args.iou)
+	if (opts.use_iou)
 		worker_iou_init(&self);
 	else
 		worker_epoll_init(&self);

commit a91654e7007e03399be8ac8a22364f9a478baeaa

server: fix unpropagated worker_opts
worker_state.{rx_mode,tx_mode,validate} are no longer updated, so
reference the mode and validate fields of worker_opts instead of
worker_state. Remove these fields from worker_state. This fixes bugs
caused by using these fields uninitialized.

Fixes: 3b4a31b4b8f2 ("worker: refactor worker_main_args to worker_opts")
Signed-off-by: Bobby Eshleman <bobbyeshleman@meta.com>

epoll.c
@@ -68,9 +68,9 @@ ep_conn_close(struct worker_state *self, struct worker_connection *conn)
 	ev.data.fd = conn->fd;
 	if (epoll_ctl(self->epollfd, EPOLL_CTL_DEL, conn->fd, &ev) < 0)
 		warn("Failed to del poll out");
-	if (self->rx_mode == KPM_RX_MODE_DEVMEM)
+	if (self->opts.rx_mode == KPM_RX_MODE_DEVMEM)
 		(void)devmem_release_tokens(conn->fd, &conn->devmem);
-	else if (self->rx_mode == KPM_RX_MODE_SOCKET_ZEROCOPY)
+	else if (self->opts.rx_mode == KPM_RX_MODE_SOCKET_ZEROCOPY)
 		munmap(conn->raddr, conn->rsize);
 }
 
@@ -80,14 +80,14 @@ ep_conn_add(struct worker_state *self, struct worker_connection *conn)
 	struct epoll_event ev = {};
 	int zc;
 
-	zc = self->tx_mode == KPM_TX_MODE_SOCKET_ZEROCOPY || self->tx_mode == KPM_TX_MODE_DEVMEM;
+	zc = self->opts.tx_mode == KPM_TX_MODE_SOCKET_ZEROCOPY || self->opts.tx_mode == KPM_TX_MODE_DEVMEM;
 	if (setsockopt(conn->fd, SOL_SOCKET, SO_ZEROCOPY, &zc, sizeof(zc))) {
 		warnx("Failed to set SO_ZEROCOPY");
 		self->quit = 1;
 		return;
 	}
 
-	if (self->rx_mode == KPM_RX_MODE_SOCKET_ZEROCOPY) {
+	if (self->opts.rx_mode == KPM_RX_MODE_SOCKET_ZEROCOPY) {
 		size_t map_align;
 
 		map_align = default_huge_page_size();
@@ -229,7 +229,7 @@ ep_handle_send(struct worker_state *self, struct worker_connection *conn,
 	       unsigned int events)
 {
 	unsigned int rep = max_t(int, 10, conn->to_send / conn->write_size + 1);
-	bool msg_zerocopy = self->tx_mode == KPM_TX_MODE_SOCKET_ZEROCOPY || self->tx_mode == KPM_TX_MODE_DEVMEM;
+	bool msg_zerocopy = self->opts.tx_mode == KPM_TX_MODE_SOCKET_ZEROCOPY || self->opts.tx_mode == KPM_TX_MODE_DEVMEM;
 	int flags = msg_zerocopy ? MSG_ZEROCOPY : 0;
 
 	while (rep--) {
@@ -239,7 +239,7 @@ ep_handle_send(struct worker_state *self, struct worker_connection *conn,
 
 		chunk = min_t(size_t, conn->write_size, conn->to_send);
 
-		if (self->tx_mode == KPM_TX_MODE_DEVMEM) {
+		if (self->opts.tx_mode == KPM_TX_MODE_DEVMEM) {
 			n = devmem_sendmsg(conn->fd, self->opts.devmem.dmabuf_id,
 					   conn->tot_sent % PATTERN_PERIOD, chunk);
 		} else {
@@ -308,7 +308,7 @@ ep_handle_zerocopy_recv(struct worker_state *self, struct worker_connection *con
 		return zc.err;
 
 	if (zc.length) {
-		if (self->validate && memcmp(conn->addr, src, zc.length))
+		if (self->opts.validate && memcmp(conn->addr, src, zc.length))
 			warnx("Data corruption %d %d %u %lld %lld %d",
 			*(char *)conn->addr, *(char *)src, zc.length,
 			conn->tot_recv % PATTERN_PERIOD,
@@ -319,7 +319,7 @@ ep_handle_zerocopy_recv(struct worker_state *self, struct worker_connection *con
 	}
 
 	if (zc.copybuf_len) {
-		if (self->validate && memcmp(conn->rxbuf, src, zc.copybuf_len))
+		if (self->opts.validate && memcmp(conn->rxbuf, src, zc.copybuf_len))
 			warnx("Data corruption %d %d %d %lld %lld %d",
 			*conn->rxbuf, *(char *)src, zc.copybuf_len,
 			(conn->tot_recv + n) % PATTERN_PERIOD,
@@ -336,7 +336,7 @@ static ssize_t
 ep_handle_regular_recv(struct worker_state *self, struct worker_connection *conn,
 		       size_t chunk, int rep)
 {
-	bool msg_trunc = self->rx_mode == KPM_RX_MODE_SOCKET_TRUNC;
+	bool msg_trunc = self->opts.rx_mode == KPM_RX_MODE_SOCKET_TRUNC;
 	void *src = &patbuf[conn->tot_recv % PATTERN_PERIOD];
 	int flags = msg_trunc ? MSG_TRUNC : 0;
 	ssize_t n;
@@ -346,7 +346,7 @@ ep_handle_regular_recv(struct worker_state *self, struct worker_connection *conn
 	if (n <= 0 || msg_trunc)
 		return n;
 
-	if (self->validate && memcmp(conn->rxbuf, src, n))
+	if (self->opts.validate && memcmp(conn->rxbuf, src, n))
 		warnx("Data corruption %d %d %ld %lld %lld %d",
 		      *conn->rxbuf, *(char *)src, n,
 		      conn->tot_recv % PATTERN_PERIOD,
@@ -365,11 +365,11 @@ ep_handle_recv(struct worker_state *self, struct worker_connection *conn)
 		ssize_t n;
 
 		chunk = min_t(size_t, conn->read_size, conn->to_recv);
-		if (self->rx_mode == KPM_RX_MODE_DEVMEM)
+		if (self->opts.rx_mode == KPM_RX_MODE_DEVMEM)
 			n = devmem_recv(conn->fd, &conn->devmem,
 					conn->rxbuf, chunk, self->opts.devmem.mem,
-					rep, conn->tot_recv, self->validate);
-		else if (self->rx_mode == KPM_RX_MODE_SOCKET_ZEROCOPY)
+					rep, conn->tot_recv, self->opts.validate);
+		else if (self->opts.rx_mode == KPM_RX_MODE_SOCKET_ZEROCOPY)
 			n = ep_handle_zerocopy_recv(self, conn, chunk, rep);
 		else
 			n = ep_handle_regular_recv(self, conn, chunk, rep);

iou.c
@@ -206,7 +206,7 @@ static void iou_handle_recv(struct worker_state *self, struct io_uring_cqe *cqe)
 	}
 
 	src = &patbuf[conn->tot_recv % PATTERN_PERIOD];
-	if (self->validate && memcmp(conn->rxbuf, src, n))
+	if (self->opts.validate && memcmp(conn->rxbuf, src, n))
 		warnx("Data corruption %d %d %ld %lld %lld",
 		      *conn->rxbuf, *(char *)src, n,
 		      conn->tot_recv % PATTERN_PERIOD,
@@ -262,7 +262,7 @@ static void iou_handle_recvzc(struct worker_state *self, struct io_uring_cqe *cq
 	data = (unsigned char *)state->area_ptr + (rcqe->off & mask);
 
 	src = &patbuf[conn->tot_recv % PATTERN_PERIOD];
-	if (self->validate && memcmp(data, src, n))
+	if (self->opts.validate && memcmp(data, src, n))
 		warnx("Data corruption %d %d %ld %lld %lld",
 		      *data, *(char *)src, n,
 		      conn->tot_recv % PATTERN_PERIOD,
@@ -397,7 +397,7 @@ static void iou_prep(struct worker_state *self)
 	p.flags |= IORING_SETUP_DEFER_TASKRUN;
 	p.flags |= IORING_SETUP_SINGLE_ISSUER;
 	p.flags |= IORING_SETUP_SUBMIT_ALL;
-	if (self->rx_mode == KPM_RX_MODE_SOCKET_ZEROCOPY)
+	if (self->opts.rx_mode == KPM_RX_MODE_SOCKET_ZEROCOPY)
 		p.flags |= IORING_SETUP_CQE32;
 	p.cq_entries = 512;
 
@@ -411,11 +411,11 @@ static void iou_prep(struct worker_state *self)
 		err(6, "Failed to malloc iou_kpm_msg_state");
 	}
 
-	if (self->rx_mode == KPM_RX_MODE_SOCKET_ZEROCOPY)
+	if (self->opts.rx_mode == KPM_RX_MODE_SOCKET_ZEROCOPY)
 		if (iou_register_zerocopy_rx(self))
 			err(7, "Failed to register zero copy rx");
 
-	if (self->tx_mode == KPM_TX_MODE_SOCKET_ZEROCOPY)
+	if (self->opts.tx_mode == KPM_TX_MODE_SOCKET_ZEROCOPY)
 		if (iou_register_zerocopy_tx(self))
 			err(8, "Failed to register zero copy tx");
 
@@ -555,13 +555,13 @@ static void iou_conn_add(struct worker_state *state, struct worker_connection *c
 	struct io_uring *ring = get_ring(state);
 
 	if (conn->to_send) {
-		if (state->tx_mode == KPM_TX_MODE_SOCKET_ZEROCOPY)
+		if (state->opts.tx_mode == KPM_TX_MODE_SOCKET_ZEROCOPY)
 			iou_conn_add_sendzc(ring, conn);
 		else
 			iou_conn_add_send(ring, conn);
 	}
 
-	if (state->rx_mode == KPM_RX_MODE_SOCKET_ZEROCOPY)
+	if (state->opts.rx_mode == KPM_RX_MODE_SOCKET_ZEROCOPY)
 		iou_conn_add_recvzc(ring, conn, get_iou_state(state)->zcrx_id);
 	else
 		iou_conn_add_recv(ring, conn);

worker.h
@@ -16,8 +16,6 @@
 struct worker_state {
 	int main_sock;
 	struct worker_opts opts;
-	enum kpm_rx_mode rx_mode;
-	enum kpm_tx_mode tx_mode;
 	int epollfd;
 	unsigned int id;
 	int quit;